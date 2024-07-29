7ef5d31c62dee1ccee7c2f292a29c81cf9141211
OsmanWu <23007753@myrp.edu.sg>
2024-07-29 12:00:51 +0800

    hi

--- public/db.js
+++ public/db.js
@@ -1,10 +1,15 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Republic_C207',
-    database: 'travelweb'
+   // host: 'localhost',
+    //user: 'root',
+    //password: 'Republic_C207',
+    //database: 'travelweb'
+
+    host: 'sql.freedb.tech',
+    user: 'freedb_Osman',
+    password: '&3C7jBZZ9sRfE9E',
+    database: 'freedb_travelweb'
 });
 
 connection.connect((err) => {
